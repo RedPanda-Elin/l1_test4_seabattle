61076c9fac6c0d6515d1f4ad5089d166cf8866f2
ElinDi <vivalatanya@gmail.com>
2017-11-16 12:44:50 +0500

    Сравнения

--- l1_test4_seabattle/src/ex4_equals/Main.java
+++ l1_test4_seabattle/src/ex4_equals/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         String string1 = "hello";
         String string2 = "hello";
-        string2 = "bye";
+//        string2 = "bye";
+        String string3 = new String("hello");
 
         System.out.println(string1 == string2);
+        System.out.println(string1 == string3);
     }
 }
